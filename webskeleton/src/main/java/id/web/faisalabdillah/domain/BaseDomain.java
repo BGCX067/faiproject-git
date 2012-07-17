817ca2c8fa06576f13f03d0f0bf86223eb2aed2b
faitz1989@gmal.com <aZ5xM2qy6kb3>
2012-07-17 10:28:38 +0700

    + Add copyright to files

--- webskeleton/src/main/java/id/web/faisalabdillah/domain/BaseDomain.java
+++ webskeleton/src/main/java/id/web/faisalabdillah/domain/BaseDomain.java
@@ -1,3 +1,18 @@
+/**
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *	 
+ */
+
 package id.web.faisalabdillah.domain;
 
 import java.io.Serializable;
@@ -5,7 +20,14 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-
+/**
+ * 
+ * id.web.faisalabdillah.domain
+ * @author
+ * Muhamad Faisal Abdillah
+ * <br/> Jul 17, 2012
+ *
+ */
 @MappedSuperclass
 public class BaseDomain implements Serializable{
 	/**
